7434fa69c239795237ddbd8970b2766dbe956f32
madleigh12 <madleigh12@ufl.edu>
2016-04-24 18:25:45 -0400

    Animations

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class IntroScript : MonoBehaviour {
+    TextMesh text;
+    Color col;
+    float time = 0.0f;
+    bool fade = false;
+	// Use this for initialization
+	void Start () {
+        text = gameObject.GetComponent<TextMesh>();
+        col = text.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        col = text.color;
+        Debug.Log(time);
+        if (fade && text.color.a >= 0.0) {
+            col.a -= 0.001f;
+            text.color = col;
+            if (col.a <= 0) {
+                fade = false;
+                col.a = 1;
+            }
+        }
+        if (time >= 12) {
+            text.text = "";
+        }
+        else if (time >= 9) {
+            text.text = "The nearest ranger is two hours away.";
+            fade = true;
+        }
+        else if (time >= 6) {
+            text.text = "It's just you and your friend.";
+            fade = true;
+        }
+        else if (time >= 3) {
+            text.text = "You've gone camping at a National Park.";
+            fade = true;
+        }
+        else
+        {
+            text.text = "";
+        }
+	}
+}
---
+++ Assets/Scripts/MainCharacter_Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacter_Controller : MonoBehaviour {
+    Animator anim;
+    private bool walking = false;
+    private CardboardHead head;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        head = Camera.main.GetComponent<StereoController>().Head;
+    }
+	
+	void Update () {
+        transform.position = head.Gaze.direction;
+        if (GameObject.Find("CardboardMain").GetComponent<Cardboard>().Triggered)
+        {
+            walking = !walking;
+            Debug.Log(speed * head.Gaze.direction * Time.deltaTime);
+            gameObject.GetComponent<Rigidbody>().transform.position += speed * head.Gaze.direction * Time.deltaTime;
+            anim.SetBool("isWalking", walking);
+        }
+    }
+}
