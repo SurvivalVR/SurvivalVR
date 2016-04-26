fbd4cba4db4f4fc0b0d6f12b704c1bafa848af45
Madison Hicks <madleigh12@ufl.edu>
2016-04-26 14:12:29 -0400

    audio and traversal speed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,32 +2,41 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-
+    public AudioSource baseCamp;
+    public AudioSource riverCamp;
+    public AudioSource mountainCamp;
     private bool walk;
     private bool walkDisabled;
     public float speed;
     private CardboardHead head;
     public GameObject character;
     private Animator anim;
+    private int mode;
 
     // Use this for initialization
     void Start () {
+        mode = 0;
         walk = false;
         walkDisabled = false;
         head = Camera.main.GetComponent<StereoController>().Head;
         anim = GameObject.Find("alanwake_awns01").GetComponent<Animator>();
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isSprinting", false);
+        baseCamp.Play();
     }
 	
     void OnTriggerEnter(Collider col) {
         if (col.tag == "ToTheMountain") {
             transform.position = new Vector3(7423.346f, -5522.4f, -117.4304f);
-            walk = false;
-            anim.SetBool("isWalking", walk);
+            mode = 0;
+            baseCamp.Stop();
+            mountainCamp.Play();
         }
         if (col.tag == "ToTheLake") {
             transform.position = new Vector3(7371.993f, -5796.313f, 2682.001f);
-            walk = false;
-            anim.SetBool("isWalking", walk);
+            mode = 0;
+            baseCamp.Stop();
+            riverCamp.Play();
         }
     }
 
@@ -39,6 +48,7 @@ public class Movement : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        Debug.Log(mode);
         if (transform.rotation.x != 0 || transform.rotation.z != 0)
         {
             transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
@@ -48,18 +58,35 @@ public class Movement : MonoBehaviour {
 
         if (GameObject.Find("CardboardMain").GetComponent<Cardboard>().Triggered && !walkDisabled)
         {
-            walk = !walk;
-            anim.SetBool("isWalking", walk);
+            if (mode == 2)
+            {
+                mode = 0;
+            }
+            else
+            {
+                mode++;
+            }
         }
-        if (walk)
+        if (mode == 0)
         {
+            GetComponent<AudioSource>().Pause();
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isSprinting", false);
+        }
+        else if (mode == 1)
+        {
+            speed = 7;
             transform.position += speed * head.Gaze.direction * Time.deltaTime;
             GetComponent<AudioSource>().UnPause();
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isSprinting", false);
             //character.GetComponent<Animator>().SetTrigger(isWalking, true);
         }
         else {
-            GetComponent<AudioSource>().Pause();
-            walk = false;
+            speed = 15;
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isSprinting", true);
+            transform.position += speed * head.Gaze.direction * Time.deltaTime;
         }
 	}
 }
