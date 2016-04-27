ee1e6d213f304a59a0f07847f3367de198a38c5b
TiffanyDixon <TiffanyDixon94@gmail.com>
2016-04-27 13:16:46 -0400

    Added more assets and heli

---
+++ Assets/HelicopterSpinning.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterSpinning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 50, 0 * Time.deltaTime);
+	}
+}
